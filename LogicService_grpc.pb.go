2e19ddd80afc0279a3de13be58548b3f6682939c
段明磊 <duanml1@yonyou.com>
2024-11-26 19:49:48 +0800

    gatewayservice

--- LogicService_grpc.pb.go
+++ LogicService_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.5.1
 // - protoc             v5.28.3
-// source: proto/LogicService/LogicService.proto
+// source: LogicService.proto
 
 package gobtphelper
 
@@ -117,5 +117,107 @@ var LogicService_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "proto/LogicService/LogicService.proto",
+	Metadata: "LogicService.proto",
+}
+
+const (
+	GatewayService_ReceiveMessage_FullMethodName = "/GatewayService/ReceiveMessage"
+)
+
+// GatewayServiceClient is the client API for GatewayService service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
+type GatewayServiceClient interface {
+	ReceiveMessage(ctx context.Context, in *ProtoMessageResult, opts ...grpc.CallOption) (*ProtoInt, error)
+}
+
+type gatewayServiceClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewGatewayServiceClient(cc grpc.ClientConnInterface) GatewayServiceClient {
+	return &gatewayServiceClient{cc}
+}
+
+func (c *gatewayServiceClient) ReceiveMessage(ctx context.Context, in *ProtoMessageResult, opts ...grpc.CallOption) (*ProtoInt, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(ProtoInt)
+	err := c.cc.Invoke(ctx, GatewayService_ReceiveMessage_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// GatewayServiceServer is the server API for GatewayService service.
+// All implementations must embed UnimplementedGatewayServiceServer
+// for forward compatibility.
+type GatewayServiceServer interface {
+	ReceiveMessage(context.Context, *ProtoMessageResult) (*ProtoInt, error)
+	mustEmbedUnimplementedGatewayServiceServer()
+}
+
+// UnimplementedGatewayServiceServer must be embedded to have
+// forward compatible implementations.
+//
+// NOTE: this should be embedded by value instead of pointer to avoid a nil
+// pointer dereference when methods are called.
+type UnimplementedGatewayServiceServer struct{}
+
+func (UnimplementedGatewayServiceServer) ReceiveMessage(context.Context, *ProtoMessageResult) (*ProtoInt, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ReceiveMessage not implemented")
+}
+func (UnimplementedGatewayServiceServer) mustEmbedUnimplementedGatewayServiceServer() {}
+func (UnimplementedGatewayServiceServer) testEmbeddedByValue()                        {}
+
+// UnsafeGatewayServiceServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to GatewayServiceServer will
+// result in compilation errors.
+type UnsafeGatewayServiceServer interface {
+	mustEmbedUnimplementedGatewayServiceServer()
+}
+
+func RegisterGatewayServiceServer(s grpc.ServiceRegistrar, srv GatewayServiceServer) {
+	// If the following call pancis, it indicates UnimplementedGatewayServiceServer was
+	// embedded by pointer and is nil.  This will cause panics if an
+	// unimplemented method is ever invoked, so we test this at initialization
+	// time to prevent it from happening at runtime later due to I/O.
+	if t, ok := srv.(interface{ testEmbeddedByValue() }); ok {
+		t.testEmbeddedByValue()
+	}
+	s.RegisterService(&GatewayService_ServiceDesc, srv)
+}
+
+func _GatewayService_ReceiveMessage_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ProtoMessageResult)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(GatewayServiceServer).ReceiveMessage(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: GatewayService_ReceiveMessage_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(GatewayServiceServer).ReceiveMessage(ctx, req.(*ProtoMessageResult))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+// GatewayService_ServiceDesc is the grpc.ServiceDesc for GatewayService service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var GatewayService_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "GatewayService",
+	HandlerType: (*GatewayServiceServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "ReceiveMessage",
+			Handler:    _GatewayService_ReceiveMessage_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "LogicService.proto",
 }
